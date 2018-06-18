cd4cda1304c8dec25d81d958e647d77e9e3f162d
Jim Blackler <jimblackler@gmail.com>
2018-06-19 00:03:39 +0100

    Use frequency array rather than MultiSet

---
+++ src/main/java/net/jimblackler/ancredible/Letters.java
@@ -0,0 +1,39 @@
+package net.jimblackler.ancredible;
+
+public class Letters {
+  static byte[] getCharacters(String string) {
+    byte[] counter = new byte[26];
+
+    for (Character c : string.toLowerCase().toCharArray()) {
+      if (c >= 'a' && c <= 'z') {
+        counter[c - 'a']++;
+      }
+    }
+    return counter;
+  }
+
+  static int count(byte[] values) {
+    int count = 0;
+    for (int idx = 0; idx != 26; idx++) {
+      count += values[idx];
+    }
+    return count;
+  }
+
+  static boolean isSubset(byte[] a, byte[] b) {
+    for (int idx = 0; idx != 26; idx++) {
+      if (b[idx] < a[idx]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  static byte[] difference(byte[] a, byte[] b) {
+    byte[] counter = new byte[26];
+    for (int idx = 0; idx != 26; idx++) {
+      counter[idx] = (byte) (a[idx] - b[idx]);
+    }
+    return counter;
+  }
+}
